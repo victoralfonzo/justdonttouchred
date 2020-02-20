a8522636c5f3439dca74133bf9237f10e3256ec0
Victor Alfonzo <victoraalfonzo@gmail.com>
2020-02-20 00:40:24 -0500

    revise

--- snow.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="snow" tilewidth="16" tileheight="16" tilecount="224" columns="16">
- <image source="../Maytch 16x16 Tilesets/tilesheet_snow.png" width="256" height="224"/>
-</tileset>
